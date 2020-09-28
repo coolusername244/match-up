867c5f35f98efbbd875dbd8775c60cfb6774e169
coolusername244 <leesheppard2404@gmail.com>
2020-09-28 17:55:13 +0000

    deleted extra card images, added sounds to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,27 @@
-$('.card').click(function(){
-    
-    $(this).toggleClass("flipped");
-});
+$('.card').addClass('flipped');
+
+
+
+
+
+class SoundSetup {
+    constructor() {
+        this.flip = new Audio('assets/audio/flip.wav');
+        this.match = new Audio('assets/audio/match.wav');
+        this.win = new Audio('assets/audio/win.mp3');
+        this.gameOver = new Audio('assets/audio/gameover.wav');
+    }
+    cardFlip() {
+        this.flip.play();
+    }
+    cardMatch() {
+        this.match.play();
+    }
+    win() {
+        this.win.play();
+    }
+    gameOver() {
+        this.gameOver.play();
+    }
+}
+
