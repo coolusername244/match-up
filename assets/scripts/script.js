9c2573eb4a9663967f84f84ca9d7696114a7a71d
coolusername244 <leesheppard2404@gmail.com>
2020-10-10 12:47:21 +0000

    added document ready function/ added card flip toggler

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,16 +1,13 @@
-$('.card').addClass('flipped');
 
-
-
-
-
-class SoundSetup {
+        //Sounds Class to harbour all sounds used for game
+class Sounds {
     constructor() {
         this.flip = new Audio('assets/audio/flip.wav');
         this.match = new Audio('assets/audio/match.wav');
         this.win = new Audio('assets/audio/win.mp3');
         this.gameOver = new Audio('assets/audio/gameover.wav');
     }
+        //Functions for sound clips
     cardFlip() {
         this.flip.play();
     }
@@ -25,3 +22,19 @@ class SoundSetup {
     }
 }
 
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+    //
+function ready() {
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+            
+        });
+    });
+}
