47682320035b046a50fc7c88e7c132e556436513
coolusername244 <leesheppard2404@gmail.com>
2020-10-18 19:30:11 +0000

    deleted console.log from feedback response

--- assets/scripts/feedback.js
+++ assets/scripts/feedback.js
@@ -5,12 +5,10 @@ function sendFeedbackEmail(feedbackForm) {
         "memory_game_feedback": feedbackForm.feedback.value     //variables used in emailJS to populate email that will be sent
     })
     .then(
-        function(response) {                                    //When a status of 200 is achieved, the console will log the 
-            console.log("Your email was sent!", response);      //success message and an alert box will notify the user of
-            alert("Your feedback was sent! Thank you!");        //the email being sent
+        function(response) {                                    //When a status of 200 is achieved, the user will be notified                                                              //success message and an alert box will notify the user of
+            alert("Your feedback was sent! Thank you!");        
         },
         function(failed) {                                      //If the email fails to send, the user will be notified
-            console.log("Your email was not sent :(", failed);
             alert("Your email was not sent");
         });
         return false;                                           //Used to ensure that the page does not refresh upon submission of email
