19b3793c5c1339183f66dc1430eab6a269902f1c
Lee <leesheppard2404@gmail.com>
2023-08-09 19:47:21 +0200

    updated the outdated emailjs code

--- assets/scripts/feedback.js
+++ assets/scripts/feedback.js
@@ -1,15 +1,19 @@
 function sendFeedbackEmail(feedbackForm) {
-    emailjs.send("gmail", "MS2", {
-        "from_name": feedbackForm.name.value,                   //variables used in emailJS to populate email that will be sent
-        "from_email": feedbackForm.email.value,                 //variables used in emailJS to populate email that will be sent
-        "memory_game_feedback": feedbackForm.feedback.value     //variables used in emailJS to populate email that will be sent
+  emailjs
+    .send('service_f2y6hce', 'template_47mnlpf', {
+      from_name: feedbackForm.name.value, //variables used in emailJS to populate email that will be sent
+      from_email: feedbackForm.email.value, //variables used in emailJS to populate email that will be sent
+      message: feedbackForm.feedback.value, //variables used in emailJS to populate email that will be sent
     })
     .then(
-        function(response) {                                    //When a status of 200 is achieved, the user will be notified                                                              //success message and an alert box will notify the user of
-            alert("Your feedback was sent! Thank you!");        
-        },
-        function(failed) {                                      //If the email fails to send, the user will be notified
-            alert("Your email was not sent");
-        });
-        return false;                                           //Used to ensure that the page does not refresh upon submission of email
+      function (response) {
+        //When a status of 200 is achieved, the user will be notified
+        alert('Your feedback was sent! Thank you!');
+      },
+      function (failed) {
+        //If the email fails to send, the user will be notified
+        alert('Your email was not sent');
+      },
+    );
+  return false; //Used to ensure that the page does not refresh upon submission of email
 }
